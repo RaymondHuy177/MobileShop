2f5d937b70abd60c1d711cfe9c000dbbf4243fc5
huy.luong <raymondhuy177@gmail.com>
2016-05-09 23:34:25 -0700

    update 10052016

--- AssignmentEC/Home.aspx.cs
+++ AssignmentEC/Home.aspx.cs
@@ -33,7 +33,6 @@ namespace AssignmentEC
                         select mobile).Take(4);
             List<MobilePhone> mobileList = data.ToList();
             string html = "";
-            string a = "";
             foreach (MobilePhone mobile in mobileList)
             {
                 html += "<div class=\"view view-second\">";
